ac225bba54711edf488e1129994e0c4236f73903
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-04-01 13:24:38 -0300

    feat: add component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,18 @@ import React from 'react'
 import * as S from './styled'
 
 function Header (): JSX.Element {
-  return <S.Title>Menu</S.Title>
+  return (
+    <S.Menu height="68px">
+        <S.NavItems>
+        <S.LinkRedirect to={'/'} data-item={'Inicio'}>
+            <li>Inicio</li>
+        </S.LinkRedirect>
+            <li>Sobre</li>
+            <li>Contato</li>
+            <li>Receitas</li>
+        </S.NavItems>
+    </S.Menu>
+  )
 }
 
 export default Header
