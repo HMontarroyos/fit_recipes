c9c123038c28d82e7d5ba32f243ccbdd2f1412b4
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-04-03 05:33:06 -0300

    chore: add React Reveal library to project

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,23 +1,50 @@
-import React from 'react'
-import * as S from './styled'
-import { Buttom } from '../../components'
+/* eslint-disable */
+import React from "react";
+import { Fade, Slide } from "react-reveal";
+import * as S from "./styled";
+import { Buttom } from "../../components";
+import plate01 from "../../assets/images/plate_of_food_01.svg";
+import plate02 from "../../assets/images/plate_of_food_02.svg";
+import plate03 from "../../assets/images/plate_of_food_03.svg";
 
 const Home: React.FC = () => {
-  return (
-        <S.Container>
-            <div>
-                <S.Title>RECEITAS FIT</S.Title>
-                <S.Paragraph>
-                    Descubra como as receitas fit podem transformar sua vida com
-                    saúde e sabor - experimente agora e sinta-se mais motivado,
-                    saudável e confiante!
-                </S.Paragraph>
-            </div>
-            <Buttom width={'300px'} height={'50px'}>
-                Transforme sua vida agora com um clique
-            </Buttom>
-        </S.Container>
-  )
-}
+    return (
+        <>
+            <S.Container>
+                <div>
+                    <S.Title>RECEITAS FIT</S.Title>
+                    <S.Paragraph>
+                        Descubra como as receitas fit podem transformar sua vida
+                        com saúde e sabor - experimente agora e sinta-se mais
+                        motivado, saudável e confiante!
+                    </S.Paragraph>
+                </div>
+                <S.LinkRedirect to={"/recipes"}>
+                    <Buttom width={"300px"} height={"50px"}>
+                        Transforme sua vida agora com um clique
+                    </Buttom>
+                </S.LinkRedirect>
+            </S.Container>
+            <S.ContainerTopRight>
+                <Fade top>
+                    <img src={plate01} />
+                </Fade>
+            </S.ContainerTopRight>
+            <S.ContainerBottomRight>
+                <Fade right>
+                    <img src={plate02} />
+                </Fade>
+            </S.ContainerBottomRight>
+            {/*             <S.ContainerBottomLeft>
 
-export default Home
+
+                <Fade left>
+                    <img src={plate03} />
+
+               </Fade>
+            </S.ContainerBottomLeft> */}
+        </>
+    );
+};
+
+export default Home;
--- src/views/Home/styled.tsx
+++ src/views/Home/styled.tsx
@@ -1,4 +1,9 @@
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
+
+export const LinkRedirect = styled(Link)`
+    text-decoration: none;
+`
 
 export const Container = styled.div`
     display: flex;
@@ -12,7 +17,6 @@ export const Container = styled.div`
         margin-bottom: 100px;
     }
 `
-
 export const Title = styled.h1`
     text-align: center;
     color: ${(props) => props.theme.colors.text};
@@ -26,4 +30,39 @@ export const Paragraph = styled.p`
     font-size: ${(props) => props.theme.fontSizes.medium};
     font-family: ${(props) => props.theme.fonts.text};
     font-weight: 200;
+`
+export const ContainerBottomRight = styled.div`
+    bottom: 0;
+    right: 0;
+    position: fixed;
+
+    img {
+        width: 351px;
+        height: 390px;
+    }
+`
+
+export const ContainerTopRight = styled.div`
+    top: 100px;
+    right: 0;
+    position: fixed;
+
+    img {
+        width: 399px;
+        height: 320px;
+        z-index:999;
+    }
+`
+
+export const ContainerBottomLeft = styled.div`
+    top: 0px;
+bottom: 0px;
+    left: 0px;
+    position: fixed;
+    z-index:-1;
+    img {
+        z-index:-1;
+        width: 600px;
+        height: 679px;
+    }
 `
