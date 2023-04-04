9588aa1e21712ed17605ca89a16c81549eff92e6
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-04-04 09:59:01 -0300

    feat: adding local api to project and creating get routes to bring recipes

---
+++ src/server/api.tsx
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3000/recipes'
+
+const api = axios.create({ baseURL: baseUrl })
+
+export const getRecipes = async (): Promise<any> => {
+  try {
+    const response = await api.get(baseUrl)
+    return response.data
+  } catch (error) {
+    console.error(error)
+    throw new Error('Error fetching recipes')
+  }
+}
--- src/views/Recipes/index.tsx
+++ src/views/Recipes/index.tsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import * as S from './styled'
+import { getRecipes } from '../../server/api'
+
+interface Recipe {
+  _id: string
+  name: string
+  description: string
+  ingredients: string[]
+  instructions: string
+}
 
 const Home: React.FC = () => {
+  const [recipes, setRecipes] = useState<Recipe[]>([])
+
+  useEffect(() => {
+    async function fetchRecipes (): Promise<void> {
+      try {
+        const recipes = await getRecipes()
+        setRecipes(recipes)
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    fetchRecipes()
+  }, [])
+
   return (
         <>
-<S.Title>RECEITAS</S.Title>
+            <S.Title>RECEITAS</S.Title>
+            <ul>
+                {recipes.map((recipe) => (
+                    <li key={recipe._id}>
+                        <h1>{recipe.name}</h1>
+                        <h2>Ingredientes</h2>
+                        {recipe.ingredients.map((ingredient) => (
+                            <p key={recipe._id}>{ingredient}</p>
+                        ))}
+                        <i>{recipe.description}</i>
+                        <h3>Modo de preparo:</h3>
+                        <p>{recipe.instructions}</p>
+                    </li>
+                ))}
+            </ul>
         </>
   )
 }
