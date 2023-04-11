21cd4eb77e52cb001b41eafe67c1c8eb8dd58c36
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-04-11 13:15:26 -0300

    chore: add image import index to assets

---
+++ src/assets/images/icons/index.tsx
@@ -0,0 +1,7 @@
+import icon01 from '../icons/icon01.svg'
+import icon02 from '../icons/icon02.svg'
+import icon03 from '../icons/icon03.svg'
+import icon04 from '../icons/icon04.svg'
+import icon05 from '../icons/icon05.svg'
+
+export { icon01, icon02, icon03, icon04, icon05 }
---
+++ src/assets/images/index.tsx
@@ -0,0 +1,9 @@
+import plate01 from '../images/plate_of_food_01.svg'
+import plate02 from '../images/plate_of_food_02.svg'
+import plate03 from '../images/plate_of_food_03.svg'
+import plate04 from '../images/plate_of_food_04.svg'
+import plate05 from '../images/plate_of_food_05.svg'
+import plate06 from '../images/plate_of_food_06.svg'
+import backgroundPolygon from '../images/background_polygon.svg'
+
+export { plate01, plate02, plate03, plate04, plate05, plate06, backgroundPolygon }
