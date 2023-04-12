e11a8fbbece9a3a5c4c5b95dda4d2666e0234672
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-04-12 11:17:23 -0300

    style: wip style view contact

--- src/views/Contact/index.tsx
+++ src/views/Contact/index.tsx
@@ -1,11 +1,41 @@
 import React from 'react'
 import * as S from './styled'
+import { Flip } from 'react-reveal'
+import { plate07 } from '../../assets/images'
 
 const Contact: React.FC = () => {
   return (
-        <>
-<S.Title>Contato</S.Title>
-        </>
+    <>
+        <S.Container>
+            <S.ContainerText>
+            <Flip top>
+                <S.Paragraph>
+                    Bem-vindo(a) à nossa página de Fale Conosco! Estamos aqui
+                    para ajudar e ficaremos super felizes em receber sua
+                    mensagem.
+                </S.Paragraph>
+                <S.Paragraph>
+                    Nossa equipe está pronta para responder às suas dúvidas,
+                    sugestões, elogios ou até mesmo para ouvir suas histórias
+                    incríveis.
+                </S.Paragraph>
+                <S.Paragraph>
+                    Além disso nossas redes sociais são perfeitas para
+                    interagir, tirar dúvidas, compartilhar experiências e enviar
+                    sugestões. Siga-nos para fazer parte da nossa comunidade
+                    animada e não perder momentos divertidos!
+                </S.Paragraph>
+                </Flip>
+            </S.ContainerText>
+            <S.ContainerForm>
+            <S.Title>CONTATE-NOS</S.Title>
+            </S.ContainerForm>
+        </S.Container>
+            <S.ContainerImage>
+                <img src={plate07} />
+            </S.ContainerImage>
+            <S.BackgroundImage />
+            </>
   )
 }
 
--- src/views/Contact/styled.tsx
+++ src/views/Contact/styled.tsx
@@ -1,5 +1,69 @@
 import styled from 'styled-components'
+import { backgroundRectangle } from '../../assets/images'
 
-export const Title = styled.h1`
+export const Container = styled.div`
+display:flex;
+flex-direction:column;
+justify-content: space-between;
+`
 
+export const Title = styled.h2`
+text-align: right;
+color: ${(props) => props.theme.colors.text};
+font-size: ${(props) => props.theme.fontSizes.medium2};
+font-family: ${(props) => props.theme.fonts.title};
+font-weight: 400;
+`
+
+export const Paragraph = styled.p`
+    margin:20px;
+    text-align: left;
+    color: ${(props) => props.theme.colors.text};
+    font-size: ${(props) => props.theme.fontSizes.medium};
+    font-family: ${(props) => props.theme.fonts.text};
+    font-weight: 200;
+
+
+`
+
+export const ContainerText = styled.div`
+width:421px;
+height:431;
+`
+
+export const ContainerForm = styled.div`
+
+`
+
+export const BackgroundImage = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-image: url(${backgroundRectangle});
+    background-repeat: no-repeat;
+    background-size: cover;
+    width: 236px;
+    height: 100%;
+    z-index: -1;
+    margin: auto;
+`
+export const ContainerImage = styled.div`
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: auto;
+    right: 0;
+    z-index: -1;
+    margin-top: auto;
+    margin-bottom: auto;
+    margin-left: auto;
+    margin-right: 0;
+
+    img {
+        height: 100%;
+        width: 100%;
+        opacity: 0.3;
+    }
 `
