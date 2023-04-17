1baef9d6d1aa16a3b7dfb88e5de461e0795a6c19
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-04-17 12:55:01 -0300

    chore: add new assets to the project

--- src/assets/images/index.tsx
+++ src/assets/images/index.tsx
@@ -1,3 +1,7 @@
+import banner01 from '../images/banner01.svg'
+import banner02 from '../images/banner02.svg'
+import banner03 from '../images/banner03.svg'
+import banner04 from '../images/banner04.svg'
 import plate01 from '../images/plate_of_food_01.svg'
 import plate02 from '../images/plate_of_food_02.svg'
 import plate03 from '../images/plate_of_food_03.svg'
@@ -9,4 +13,4 @@ import backgroundPolygon from '../images/background_polygon.svg'
 import backgroundContact from '../images/background_contact.svg'
 import backgroundRectangle from '../images/background_rectangle.svg'
 
-export { plate01, plate02, plate03, plate04, plate05, plate06, plate07, backgroundPolygon, backgroundContact, backgroundRectangle }
+export { banner01, banner02, banner03, banner04, plate01, plate02, plate03, plate04, plate05, plate06, plate07, backgroundPolygon, backgroundContact, backgroundRectangle }
