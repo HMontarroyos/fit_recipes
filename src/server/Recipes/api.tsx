4eda66911ac15332fc98e33f33644604889ecf47
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-04-17 22:34:45 -0300

    feat: Add new base URL to API in deployment

--- src/server/Recipes/api.tsx
+++ src/server/Recipes/api.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3000/recipes'
+const baseUrl = 'https://api-fit-recipes.vercel.app/recipes'
 const api = axios.create({ baseURL: baseUrl })
 
 export const getRecipes = async (): Promise<any> => {
