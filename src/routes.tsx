325d272260eb08514b628f11fbb4d3cf1baa3169
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-04-21 17:26:01 -0300

    feat: add React Lazy and Suspense to application routes for code splitting and loading

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { BrowserRouter, Routes as Router, Route } from 'react-router-dom'
 
-import { Home, Recipes, RecipeId, About, Contact } from './views'
-import { Header } from './components'
+/* import { Header } from './components' */
+/* import { Home, Recipes, RecipeId, About, Contact } from './views' */
+
+const Header = lazy(async () => await import('./components/Header'))
+
+const Home = lazy(async () => await import('./views/Home'))
+const Recipes = lazy(async () => await import('./views/Recipes'))
+const RecipeId = lazy(async () => await import('./views/RecipeId'))
+const About = lazy(async () => await import('./views/About'))
+const Contact = lazy(async () => await import('./views/Contact'))
 
 const Routes: React.FC = () => {
   return (
         <BrowserRouter>
-            <Header />
-            <Router>
-                <Route path="/" element={<Home />} />
-                <Route path="/recipes" element={<Recipes />} />
-                <Route path="/recipes/:id" element={<RecipeId />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/contact" element={<Contact />} />
-            </Router>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Header />
+                <Router>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/recipes" element={<Recipes />} />
+                    <Route path="/recipes/:id" element={<RecipeId />} />
+                    <Route path="/about" element={<About />} />
+                    <Route path="/contact" element={<Contact />} />
+                </Router>
+            </Suspense>
         </BrowserRouter>
   )
 }
