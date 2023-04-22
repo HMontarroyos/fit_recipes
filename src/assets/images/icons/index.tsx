651c0fd44d030e3a2a8e42891663d135bd588cd5
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-04-22 18:52:32 -0300

    feat: add icons to assets

--- src/assets/images/icons/index.tsx
+++ src/assets/images/icons/index.tsx
@@ -3,5 +3,8 @@ import icon02 from '../icons/icon02.svg'
 import icon03 from '../icons/icon03.svg'
 import icon04 from '../icons/icon04.svg'
 import icon05 from '../icons/icon05.svg'
+import icon06 from '../icons/icon06.svg'
+import icon07 from '../icons/icon07.svg'
+import icon08 from '../icons/icon08.svg'
 
-export { icon01, icon02, icon03, icon04, icon05 }
+export { icon01, icon02, icon03, icon04, icon05, icon06, icon07, icon08 }
