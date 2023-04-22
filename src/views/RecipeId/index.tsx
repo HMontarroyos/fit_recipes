f8de288dccc1b62c616f4c9d35e87fc026698c5c
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-04-22 18:55:48 -0300

    refactor: adjust recipe id screen to match Figma design and add new functionalities

--- src/views/RecipeId/index.tsx
+++ src/views/RecipeId/index.tsx
@@ -2,16 +2,37 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from "react-router-dom";
 import { getRecipeId } from '../../server/index'
+import { Tag } from "../../components"
+import { icon06, icon07, icon08} from "../../assets/images/icons"
 import * as S from './styled'
 
 
 interface Recipe {
-  _id: string
-  name: string
-  description: string
-  ingredients: string[]
-  instructions: string
-  image: string
+    name: string;
+    description?: string;
+    summary: string;
+    ingredients: {
+      checked: boolean;
+      amount?: string;
+      name: string;
+      measurement?: string;
+    }[];
+    instructions: {
+      step: string;
+      description: string;
+    }[];
+    image: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+    rating?: number;
+    timer: string;
+    portion: string;
+    difficulty: string;
+    type: string;
+    occasions: string;
+    specialOccasions?: string[];
+    utensils?: string[];
+    foodRestriction?: string[];
 }
 
 const RecipeId: React.FC = () => {
@@ -23,6 +44,7 @@ const RecipeId: React.FC = () => {
           try {
             if(id){
                 const recipe = await getRecipeId(id)
+                console.log("RECIPE", recipe)
                 setRecipe(recipe)
             }else{
                 throw new Error(`id recipe invalid`)
@@ -35,12 +57,99 @@ const RecipeId: React.FC = () => {
     fetchRecipe()
   }, [])
 
+  const handleIngredientChange = (e: React.ChangeEvent<HTMLInputElement>, ingredientId: number) => {
+    if (recipe) {
+        const updatedIngredients = [...recipe.ingredients]
+        updatedIngredients[ingredientId].checked = e.target.checked;
+        setRecipe(prevRecipe => ({...prevRecipe, ingredients: updatedIngredients}) as Recipe);
+    } else {
+        return
+    }
+  }
+
 
 
   return (
-        <>
+        <S.Container>
         <S.Title>{recipe?.name}</S.Title>
-        </>
+        {recipe?.foodRestriction && (
+            <S.ContainerTag>
+                {recipe.foodRestriction.map((foodRestriction, idx) => (
+                    <div key={idx}>
+                        <Tag text={foodRestriction}/>
+                    </div>
+                ))}
+            </S.ContainerTag>
+        )}
+        {recipe?.description && (
+            <S.Text>{recipe?.description}</S.Text>
+        )}
+        <S.ContainerImage>
+            <img src={recipe?.image} alt={recipe?.name}/>
+            <S.ContainerRecipeInformation>
+                <div>
+
+                </div>
+                <div>
+                    <S.TextAndIcon>
+                        <img src={icon06}/>
+                        <p>{recipe?.portion}</p>
+                    </S.TextAndIcon>
+                    <S.TextAndIcon>
+                        <img src={icon07}/>
+                        <p>{recipe?.timer}</p>
+                    </S.TextAndIcon>
+                    <S.TextAndIcon>
+                        <img src={icon08}/>
+                        <p>{recipe?.difficulty}</p>
+                    </S.TextAndIcon>
+                </div>
+            </S.ContainerRecipeInformation>
+        </S.ContainerImage>
+        <S.Summary>{recipe?.summary}</S.Summary>
+        <div style={{display: "flex", flexDirection: "row", justifyContent: "space-between"}}>
+            <div>
+                    <S.Label>Ingredientes</S.Label>
+                    <S.ContainerIngredients>
+                        {recipe?.ingredients.map((ingredient, idx)=> (
+                            <div key={idx}>
+                                <input
+                                    type="checkbox"
+                                    checked={!!ingredient.checked}
+                                    onChange={(e) => handleIngredientChange(e, idx)}
+                                />
+                                <label>{`${ingredient.amount} ${ingredient.measurement} ${ingredient.name}`}</label>
+                            </div>
+                        ))}
+                    </S.ContainerIngredients>
+            {/*         <select value={value} onChange={onChange}>
+                        {options.map(option => (
+                            <option key={option.value} value={option.value}>
+                                {option.label}
+                            </option>
+                        ))}
+                    </select> */}
+
+            </div>
+            {recipe?.utensils && recipe.utensils?.length > 0 && (
+                <S.ContainerUtensils>
+                    <S.Label>Utensílios</S.Label>
+                        {recipe.utensils.map((utensil, idx)=> (
+                            <S.Text key={idx}>{utensil}</S.Text>
+                        ))}
+                </S.ContainerUtensils>
+            )}
+            </div>
+        <S.Label>Modo de preparo</S.Label>
+        <S.ContainerInstructionsRecipe>
+            {recipe?.instructions.map((instruction, idx) => (
+            <div key={idx}>
+                <S.Number>{instruction.step}</S.Number>
+                <S.Text>{instruction.description}</S.Text>
+            </div>
+            ))}
+        </S.ContainerInstructionsRecipe>
+        </S.Container>
   )
 }
 
--- src/views/RecipeId/styled.tsx
+++ src/views/RecipeId/styled.tsx
@@ -1,17 +1,151 @@
 import styled from 'styled-components'
 
+export const Container = styled.div`
+    padding: 20px;
+    margin: 20px;
+`
+export const Label = styled.h3`
+    color: ${(props) => props.theme.colors.text};
+    font-size: ${(props) => props.theme.fontSizes.medium2};
+    font-family: ${(props) => props.theme.fonts.title};
+    font-weight: 700;
+    text-align: left;
+    margin-bottom: 30px;
+    margin-top: 30px;
+
+`
+
 export const Title = styled.h1`
+    color: ${(props) => props.theme.colors.text};
+    font-size: ${(props) => props.theme.fontSizes.large};
+    font-family: ${(props) => props.theme.fonts.title};
+    font-weight: 400;
+    text-align: left;
+    margin-bottom: 20px;
+`
+export const ContainerTag = styled.div`
+    display: flex;
+    flex-direction: row;
+
+    div{
+        margin: 5px;
+    }
+`
+export const Text = styled.p`
+    text-align: left;
+    color: ${(props) => props.theme.colors.text};
+    font-size: ${(props) => props.theme.fontSizes.medium};
+    font-family: ${(props) => props.theme.fonts.text};
+    font-weight: 400;
+    margin-top: 20px;
+`
+
+export const ContainerImage = styled.div`
+    width: 100%;
+    height: 661px;
+    background-color: ${(props) => props.theme.colors.background2};
+    margin-top: 30px;
+    border-radius:20px;
+
+    img {
+        width: 100%;
+        height: 558px;
+        border-radius:20px 20px 0 0 ;
+    }
+`
+export const ContainerRecipeInformation = styled.div`
+    padding-top: 30px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+    div{
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+`
+
+export const TextAndIcon = styled.div`
+    padding-right: 20px;
+    img {
+        width: 37px;
+        height: 37px;
+        padding-right: 10px;
+    }
+
+    p {
+        margin-right: 10px;
+        font-size: ${(props) => props.theme.fontSizes.medium};
+        font-family: ${(props) => props.theme.fonts.title};
+        font-weight: 700;
+    }
+
+`
+
+export const Summary = styled.p`
+    text-align: center;
+    color: ${(props) => props.theme.colors.text};
+    font-size: ${(props) => props.theme.fontSizes.medium};
+    font-family: ${(props) => props.theme.fonts.text};
+    font-weight: 400;
+    margin-top: 20px;
+    font-style: italic;
+    margin: 35px;
+`
+export const ContainerInstructionsRecipe = styled.div`
+
+    div {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+`
+
+export const Number = styled.p`
     color: ${(props) => props.theme.colors.text};
     font-size: ${(props) => props.theme.fontSizes.big};
     font-family: ${(props) => props.theme.fonts.title};
-    font-weight: 400;
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
+    font-weight: 700;
+    text-align: left;
+    margin-right: 20px;
+`
+export const ContainerUtensils = styled.div`
+    width: 384px;
+    height: 237px;
+    background-color: ${(props) => props.theme.colors.secondary};
+    border-radius:20px;
     display: flex;
+    flex-direction: column;
     align-items: center;
-    justify-content: center;
-    z-index: -1;
+    padding: 20px;
+`
+export const ContainerIngredients = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  div {
+    margin-bottom: 10px;
+  }
+
+  label {
+    color: ${(props) => props.theme.colors.text};
+    font-size: ${(props) => props.theme.fontSizes.medium};
+    font-family: ${(props) => props.theme.fonts.text};
+    font-weight: 400;
+  }
+
+  input {
+    cursor: pointer;
+    width: 24px;
+    height: 24px;
+    border-radius: 10px;
+    margin-right: 20px;
+  }
+
+  input:checked + label {
+    color: ${(props) => props.theme.colors.disabledText};
+    opacity: 0.50;
+    text-decoration: line-through;
+  }
 `
